ff08786a16814ad304dbe6e12f5750836be1daa0
Roger <Roger@Roger-PC>
2017-01-04 12:53:48 -0300

    Aplicada la bitácora para el webbapp

--- zk-entrega-definitiva/src/main/java/org/test/zk/constants/CConstantes.java
+++ zk-entrega-definitiva/src/main/java/org/test/zk/constants/CConstantes.java
@@ -1,7 +1,49 @@
 package org.test.zk.constants;
+
+import java.io.File;
+
 public class CConstantes {
 public static final String Database_Connection_Session_Key = "database";
 public static final String WEB_INF_Dir ="WEB-INF";
 public static final String Config_Dir = "config";
 public static final String Database_Connection_Config_File_Name = "database.config.xml";
+//Declaración de todas las contantes usadas en el sistema
+
+public static final String _Libs_Dir = "libs" + File.separator;
+public static final String _Lib_Ext = ".jar";
+
+public static final String _Langs_Dir = "langs" + File.separator;
+public static final String _Lang_Ext = "init.lang";
+public static final String _Common_Lang_File = "Common.init.lang";
+
+public static final String _Logs_Dir = "logs" + File.separator;
+
+public static final String _Temp_Dir = "temp" + File.separator;
+
+public static final String _Security_Dir = "security" + File.separator;
+
+public static final String _System_Dir = "system" + File.separator;
+
+public static final String _WEB_INF_Dir = "WEB-INF" + File.separator;
+
+public static final String _Config_Dir = "config" + File.separator;
+
+public static final String _Database_Connection_Config_File_Name = "database.config.xml";
+
+public static final String _Database_Connection_Production_Config_File_Name = "database.production.config.xml";
+
+public static final String _Logger_Config_File_Name = "logger.config.xml";
+
+public static final String _Logger_Config_Production_File_Name = "logger.production.config.xml";
+
+public static final String _User_Unknown = "unknown@unknown.com";
+
+public static final String _DB_Connection_Session_Key = "dbConnection";
+public static final String _User_Credential_Session_Key = "userCredential";
+public static final String _Login_Date_Time_Session_Key = "loginDateTime";
+public static final String _Log_Path_Session_Key = "logPath";
+
+public static final String _Webapp_Logger_App_Attribute_Key = "webAppLogger";
+public static final String _Webapp_Logger_Name = "webapplogger";
+public static final String _Webapp_Logger_File_Log = "webapplogger.log";
 }
